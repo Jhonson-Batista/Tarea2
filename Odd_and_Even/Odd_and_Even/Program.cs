ee3255862f888e28f047ff3a6ec9ce7df39508eb
Jhonson-Batista <jhonsonbatista17@gmail.com>
2026-02-10 18:08:04 -0400

    Tarea2

---
+++ Odd_and_Even/Odd_and_Even/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Even number");
+        }
+        else
+        {
+            Console.WriteLine("Odd number");
+
+        }
+
+        Console.ReadLine();
+    }
+}
